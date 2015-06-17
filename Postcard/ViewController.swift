e9ab27999951f551b3c4d279b5d7ccc09c0edd6d
munkyzas <audriusja0@gmail.com>
2015-06-17 22:52:45 +0300

    Added a comment to see if it works

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Testing new comits
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
